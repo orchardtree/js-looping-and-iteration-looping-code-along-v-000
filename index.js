caa40f64347347ce2126ab141d2b22e079900c9e
Jesse Wenger <wengerj@gmail.com>
2021-05-18 17:29:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const cards = []
+function writeCards(recipients, event) {
+  for (let i = 0; i < recipients.length; i++) {
+    cards[i] = (`Thank you, ${recipients[i]}, for the wonderful ${event} gift!`);
+  }
+return cards;
+}
+
+function countDown(int) {
+  for (let i = int; i > -1; i--) {
+    console.log(i);
+  }
+
+}
