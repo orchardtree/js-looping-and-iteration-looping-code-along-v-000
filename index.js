58d62f83217cf23226bd4a9a7d18c3baa08a0d6d
Jesse Wenger <wengerj@gmail.com>
2021-05-18 15:28:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function writeCards(recipients) {
   for (let i = 0; i < recipients.length; i++) {
     console.log(`Thank you, ${recipient[i]}, for the wonderful surprise gift!`);
   }
-  
+
   return recipients;
 }
 
