f92b3f76a98d83c9fdc90c8d9751e8983ce04577
Jesse Wenger <wengerj@gmail.com>
2021-05-18 15:48:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function writeCards(recipients, event) {
   for (let i = 0; i < recipients.length; i++) {
     cards[i] = (`Thank you, ${recipients[i]}, for the wonderful ${event} gift!`);
   }
-
+return cards;
 }
 
 writeCards(["Lisa", "Kaitlin", "Jan"], "surprise");
