157b823cf0ffb6127df800b00d0fe4308cbade94
Jesse Wenger <wengerj@gmail.com>
2021-05-18 15:37:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 // Code your solutions in this file
-const recipients = ["Lisa", "Kaitlin", "Jan"];
-
-function writeCards(recipients) {
+function writeCards(recipients, event) {
   for (let i = 0; i < recipients.length; i++) {
-    console.log(`Thank you, ${recipients[i]}, for the wonderful surprise gift!`);
+    console.log(`Thank you, ${recipients[i]}, for the wonderful ${event} gift!`);
   }
 
 }
 
-writeCards(recipients);
+writeCards(["Lisa", "Kaitlin", "Jan"], "surprise");
